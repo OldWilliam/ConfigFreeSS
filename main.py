597af43603699bf8cc8680ea8b7278c90a811376
weixin <2872143011@qq.com>
2016-06-03 15:54:17 +0800

    haha

--- main.py
+++ main.py
@@ -15,41 +15,43 @@ def perform():
     sec = page[index:11556]
     parser.feed(sec)
     datas = parser.get_datas()
-    datas = filter(has_mao,datas)
-    datas = map(cut_uses,datas)
+    datas = filter(has_colon,datas)
+    datas = map(cut_after_colon,datas)
     datas = filter(not_empty,datas)
+
     print '\n\n######key data has been combined to a list######'
     for i in datas:
         print i
-    new_server_cfg = handle_pack(datas)
+
+    #transform the list to the correct format that a list contain three dic
+    new_server_cfg = transform(datas)
+
+    #complete config file include useless item from the disk(type:dict)
     cfg_dic = retrieve_config()
-    print '\n\n######old config file retrieved######'
-    #Complete Cofig file include useless item(type:dict)
-    print cfg_dic
-    #The Key Server Config(type:list)
-    print '\n\n######The Key Server Config######'
+
+    #key SS config Segment(type:list)
     server_cfg = cfg_dic['configs']
-    for i in server_cfg:
-        print i
+
+    #clear the list of server config
     le = len(server_cfg)
     for j in range(le):
         server_cfg.pop(le-j-1)
+
+    #add the new server config
     server_cfg.extend(new_server_cfg)
-    print'\n\n######test'
+    print'\n\n######Finally Config File######'
     print cfg_dic
 
-
-
-
-    return datas
+    #persistent config file
+    persistent_config(cfg_dic):
 
 def not_empty(s):
     return s and s.strip()
 
-def has_mao(s):
+def has_colon(s):
     return ':' in s
 
-def cut_uses(s):
+def cut_after_colon(s):
     return s[s.index(':')+1:]
 
 def retrieve_config(path=config_path):
@@ -59,11 +61,13 @@ def retrieve_config(path=config_path):
     dict_cfg = json.loads(f)
     return dict_cfg
 
-def opersistent_config(cfg_dic):
-    fp = open(path,'w')
-    fp.write()
+def persistent_config(cfg_dic):
+    f = json.dumps(cfg_dic)
+    fp = open('haha.txt','w')
+    fp.write(f)
+    fp.close()
 
-def handle_pack(datas):
+def transform(datas):
     tag=["server","server_port","password","method"]
     d1 ={"remarks":""}
     d2 ={"remarks":""}
@@ -72,7 +76,7 @@ def handle_pack(datas):
     for i in range(3):
         for j in range(4):
             l[i][tag[j]]=datas[j]
-    print '\n\n######The New'
+    print '\n\n######New Server Config Generate######'
     print l
     return l
 
