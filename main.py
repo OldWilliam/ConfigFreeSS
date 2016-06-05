5b9ee397572288f75783bf417559707868f26773
weixin <2872143011@qq.com>
2016-06-05 18:20:00 +0800

    add gen phone config file

--- main.py
+++ main.py
@@ -40,10 +40,7 @@ def cut_after_colon(s):
 
 def pc_config(datas):
     os.system('taskkill /im Shadowsocks.exe /T /F')
-#    if os.name == 'posix':
-#        config_path = cygwin_path
-#    if os.name == 'nt':
-        config_path = win_path
+    config_path = win_path
 
     #transform the list to the correct format that a list contain three dic
     new_server_cfg = transform(datas)
